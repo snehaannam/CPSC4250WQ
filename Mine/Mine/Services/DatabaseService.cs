34b226fd1bbf0faf5b7abc998b8416b2891b927f
Sneha <annammohansn@seattleu.edu>
2020-02-12 14:09:30 -0800

    Added wipedatalist method

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -106,5 +106,11 @@ namespace Mine.Services
         {
             return Database.Table<ItemModel>().ToListAsync();
         }
+
+        public void WipeDataList()
+        {
+            Database.DropTableAsync<ItemModel>().GetAwaiter().GetResult();
+            Database.CreateTablesAsync(CreateFlags.None, typeof(ItemModel)).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
     }
 }
