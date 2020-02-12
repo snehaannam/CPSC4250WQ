ae88d7422c239284ae5a5bee0c0e91209b03926e
Sneha <annammohansn@seattleu.edu>
2020-02-12 13:58:59 -0800

    getting indexview model instance at the start of the application

--- Mine/Mine/App.xaml.cs
+++ Mine/Mine/App.xaml.cs
@@ -17,6 +17,7 @@ namespace Mine
             InitializeComponent();
 
             //DependencyService.Register<MockDataStore>();
+            var instance = ViewModels.ItemIndexViewModel.Instance;
 
             // Call the Main Page to open
             MainPage = new MainPage();
